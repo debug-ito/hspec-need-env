95719b0619589fb097c0a5240ffd51684bb84392
Toshio Ito <debug.ito@gmail.com>
2018-07-12 21:18:01 +0900

    Add skeleton of needEnv and wantEnv.

--- src/Test/Hspec/NeedEnv.hs
+++ src/Test/Hspec/NeedEnv.hs
@@ -4,5 +4,20 @@
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module Test.Hspec.NeedEnv
-       () where
+       ( needEnv,
+         wantEnv
+       ) where
 
+-- | Return value of the specified environment variable. If that
+-- environment variable is not set or it fails to parse the value, it
+-- signals failure in hspec.
+needEnv :: Read a
+        => String -- ^ name of the environment variable
+        -> IO a -- ^ value of the environment variable
+needEnv = undefined
+
+-- | Like 'needEnv', but this functions signals \"pending\" in hsepc.
+wantEnv :: Read a
+        => String -- ^ name of the environment variable
+        -> IO a -- ^ value of the environment variable
+wantEnv = undefined
